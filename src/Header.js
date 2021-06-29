de12a7432c69990835c8ba8338b9eeb57612d592
Subash466 <2812subash@gmail.com>
2021-06-29 14:05:50 +0530

    style updated

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './index.css'
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className='header img-responsive'>
       <img src={img1} alt='logo'/>
       <h1>Meme Generator</h1>
     </div>
